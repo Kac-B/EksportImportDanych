f33b1154547222650ce83008a701c4c8b3ea0aef
Kac-B <51192801+Kac-B@users.noreply.github.com>
2019-12-26 09:28:08 +0100

    Update tables nbp.py

--- tables nbp.py
+++ tables nbp.py
@@ -1,17 +1,11 @@
 #-*- coding: utf-8 -*-
-import pandas as pd
-import numpy as np
-from pandas import Series, DataFrame
-import requests
-import lxml.html as lh
-import html5lib
-import bs4
 from datetime import date
-import time
 from pandas.io.html import read_html
+from openpyxl import Workbook
+import csv
 
 page='https://www.nbp.pl/home.aspx?f=/kursy/kursya.html'
-infobox=read_html(page, index_col=0,attrs={'class':'pad5'})
+infobox=read_html(page, index_col=0,attrs={'class':'pad5'}, encoding="utf-8")
 
 data=date.today()
 
@@ -19,3 +13,9 @@ file_save=('C:/Users/ja/Documents/Python/download data/tabela_kursow_walut_'+str
 
 infobox[0].to_csv(file_save,sep='\t' )
 print(file_save)
+wb=Workbook()
+ws=wb.active
+with open('C:/Users/ja/Documents/Python/download data/tabela_kursow_walut_'+str(data)+'.csv','r',encoding='utf-8') as f:
+    for row in csv.reader(f):
+        ws.append(row)
+wb.save('tabela_kursow_walut'+str(data)+'.xlsx')
